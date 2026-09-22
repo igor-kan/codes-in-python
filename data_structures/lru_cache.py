3e021c39b753f35135ebe1490e2424b548bb6261
igor-kan <igorkan010@gmail.com>
2026-09-22 13:10:52 -0400

    feat: implement lru_cache.py

---
+++ data_structures/lru_cache.py
@@ -0,0 +1,31 @@
+"""Least-recently-used cache built on a doubly linked list."""
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.store: OrderedDict = OrderedDict()
+
+    def get(self, key):
+        if key not in self.store:
+            return -1
+        self.store.move_to_end(key)
+        return self.store[key]
+
+    def put(self, key, value) -> None:
+        if key in self.store:
+            self.store.move_to_end(key)
+        self.store[key] = value
+        if len(self.store) > self.capacity:
+            self.store.popitem(last=False)
+
+
+if __name__ == "__main__":
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1
+    cache.put(3, 3)
+    assert cache.get(2) == -1
+    print("ok")
