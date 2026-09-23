29c1a44462501bcb1e41fe52cd4eadf93ed3b71e
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:18 -0400

    feat: implement fibonacci_41.py

---
+++ maths/fibonacci_41.py
@@ -0,0 +1,11 @@
+"""Fibonacci number 41."""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+if __name__ == "__main__":
+    assert fibonacci(41) == 165580141
+    print("ok")
