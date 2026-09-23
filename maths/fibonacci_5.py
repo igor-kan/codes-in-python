7662ba10756f8e844ad3a5b6eee8d2f7e9e4ffc7
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:18 -0400

    feat: implement fibonacci_5.py

---
+++ maths/fibonacci_5.py
@@ -0,0 +1,11 @@
+"""Fibonacci number 5."""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+if __name__ == "__main__":
+    assert fibonacci(5) == 5
+    print("ok")
