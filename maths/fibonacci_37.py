5eb7ba687c33551ff10ec2d3457b90753db7fe89
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:18 -0400

    feat: implement fibonacci_37.py

---
+++ maths/fibonacci_37.py
@@ -0,0 +1,11 @@
+"""Fibonacci number 37."""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+if __name__ == "__main__":
+    assert fibonacci(37) == 24157817
+    print("ok")
