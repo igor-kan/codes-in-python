bfed1d116e84b1b4fb0b7b046fa25bc36b143b2a
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:18 -0400

    feat: implement fibonacci_23.py

---
+++ maths/fibonacci_23.py
@@ -0,0 +1,11 @@
+"""Fibonacci number 23."""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+if __name__ == "__main__":
+    assert fibonacci(23) == 28657
+    print("ok")
