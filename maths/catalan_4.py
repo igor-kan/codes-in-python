9f1015b3011a6e95ccafad53770b46512a74ac61
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:15 -0400

    feat: implement catalan_4.py

---
+++ maths/catalan_4.py
@@ -0,0 +1,9 @@
+"""Catalan number 4."""
+
+def catalan(n):
+    from math import comb
+    return comb(2 * n, n) // (n + 1)
+
+if __name__ == "__main__":
+    assert catalan(4) == 14
+    print("ok")
