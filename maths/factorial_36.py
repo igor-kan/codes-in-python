8907e1e9906eec0c869bd7965b5bdeaf5081d996
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:17 -0400

    feat: implement factorial_36.py

---
+++ maths/factorial_36.py
@@ -0,0 +1,11 @@
+"""Factorial of 36."""
+
+def factorial(n):
+    r = 1
+    for i in range(2, n + 1):
+        r *= i
+    return r
+
+if __name__ == "__main__":
+    assert factorial(36) == 371993326789901217467999448150835200000000
+    print("ok")
