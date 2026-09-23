4f481cc0fd1ed8c52a01632bd31e4261fd68622f
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:16 -0400

    feat: implement factorial_11.py

---
+++ maths/factorial_11.py
@@ -0,0 +1,11 @@
+"""Factorial of 11."""
+
+def factorial(n):
+    r = 1
+    for i in range(2, n + 1):
+        r *= i
+    return r
+
+if __name__ == "__main__":
+    assert factorial(11) == 39916800
+    print("ok")
