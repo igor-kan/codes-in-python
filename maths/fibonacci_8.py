f75531cdda44ee3ba10be9751068b818e8d4e678
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:18 -0400

    feat: implement fibonacci_8.py

---
+++ maths/fibonacci_8.py
@@ -0,0 +1,11 @@
+"""Fibonacci number 8."""
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+if __name__ == "__main__":
+    assert fibonacci(8) == 21
+    print("ok")
