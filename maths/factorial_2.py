0ca03769d4d0fd8f4bdb05ae11a09404031ca6a0
igor-kan <igorkan010@gmail.com>
2026-09-23 00:34:16 -0400

    feat: implement factorial_2.py

---
+++ maths/factorial_2.py
@@ -0,0 +1,11 @@
+"""Factorial of 2."""
+
+def factorial(n):
+    r = 1
+    for i in range(2, n + 1):
+        r *= i
+    return r
+
+if __name__ == "__main__":
+    assert factorial(2) == 2
+    print("ok")
