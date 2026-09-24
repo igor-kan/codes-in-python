8d7a800dc4a2e8eeb566640ec493c6e183b3bb80
igor-kan <igorkan010@gmail.com>
2026-09-24 09:17:32 -0400

    feat(spectral_pde_and_pseudospectral): implement Rayleigh Benard Convection Rolls algorithm

---
+++ spectral_pde_and_pseudospectral/rayleigh_benard_convection_rolls.py
@@ -0,0 +1,7 @@
+"""Critical Rayleigh Number for Onset of Thermal Convection Rolls."""
+import numpy as np
+
+
+def critical_rayleigh_number_rigid() -> float:
+    """Critical Ra for rigid-rigid conducting boundaries Ra_c approx 1707.76."""
+    return 1707.76
